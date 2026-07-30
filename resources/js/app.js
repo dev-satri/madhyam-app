9922d5d5259581ab65721effe24368ec6a78235e
Taiyo_11/26 <rlama2603@gmail.com>
2026-07-30 12:21:12 +0545

    Fixed chunks for build

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,9 @@ import './attachment-preview';
 
 import Chart from 'chart.js/auto';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
-import Sortable from 'sortablejs';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 Chart.register(ChartDataLabels);
 
 window.Chart = Chart;
-window.Sortable = Sortable;
--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,21 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true
         })
-    ]
+    ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    chart: ['chart.js/auto', 'chartjs-plugin-datalabels'],
+                    tiptap: [
+                        '@tiptap/core',
+                        '@tiptap/starter-kit',
+                        '@tiptap/extension-image',
+                        '@tiptap/extension-link',
+                        '@tiptap/extension-placeholder'
+                    ]
+                }
+            }
+        }
+    }
 });
