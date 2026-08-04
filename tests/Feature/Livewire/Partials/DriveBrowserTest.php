d2f1abcbc9ff4a634373b5bcc368d80995006686
Taiyo_11/26 <rlama2603@gmail.com>
2026-08-04 11:21:42 +0545

    fix: add RefreshDatabase to DriveBrowserTest so google_drive_connections table exists

--- tests/Feature/Livewire/Partials/DriveBrowserTest.php
+++ tests/Feature/Livewire/Partials/DriveBrowserTest.php
@@ -3,11 +3,14 @@
 namespace Tests\Feature\Livewire\Partials;
 
 use App\Livewire\Partials\DriveBrowser;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Livewire;
 use Tests\TestCase;
 
 class DriveBrowserTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_renders_successfully()
     {
         Livewire::test(DriveBrowser::class)
